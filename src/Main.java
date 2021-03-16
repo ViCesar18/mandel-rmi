800343cf7e79ce1fadbcc8265d6aaaac86882368
Vinicius <vinicius.cesar@uel.br>
2021-03-16 11:06:04 -0300

    Adicionando a main

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Main {
+
+  public static void main(String[] args) {
+    MandelBrotApplet applet = new MandelBrotApplet();
+
+    JFrame frame = new JFrame();
+    frame.setSize(new Dimension(640, 480));
+    frame.setTitle("MandelBrot");
+    frame.add(applet);
+    frame.setFocusable(true);
+    frame.setVisible(true);
+
+    applet.init();
+    applet.start();
+  }
+}
